8f017a92b94a2d0c71dbac6e3dc73e532fb3a050
aldofahrezy <aldofhrzy@gmail.com>
2026-05-22 18:49:25 +0700

    fix: register BouncyCastle provider at startup for web-push

--- src/main/java/id/ac/ui/cs/advprog/bidmartordernotificationservice/OrderAndNotificationServiceApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/bidmartordernotificationservice/OrderAndNotificationServiceApplication.java
@@ -1,12 +1,21 @@
 package id.ac.ui.cs.advprog.bidmartordernotificationservice;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.security.Security;
+
 @SpringBootApplication
 @EnableScheduling
 public class OrderAndNotificationServiceApplication {
+
+    static {
+        if (Security.getProvider(BouncyCastleProvider.PROVIDER_NAME) == null) {
+            Security.addProvider(new BouncyCastleProvider());
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(OrderAndNotificationServiceApplication.class, args);
